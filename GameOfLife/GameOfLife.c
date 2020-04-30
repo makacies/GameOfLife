cc38bf073091cf43dc4d4c48a81d62655cd4d9ea
makacies <barberrywork@gmail.com>
2020-04-30 21:41:12 +0300

    Build the next generation

--- GameOfLife/GameOfLife.c
+++ GameOfLife/GameOfLife.c
@@ -1,7 +1,9 @@
 //1. + Represent generation state
 //2. - Generate a random start position
-//3. Print the first generation
+//3. + Print the first generation
 //4. Go through the board to build the next generation (Examine nine cells group and set state of the inner cell)
+//5. Jump to the opposite side of console when the figure reaches the corner
+//6. Optimaze not to check neighbourhood that didn't change
 #include <stdio.h>
 #include <stdbool.h>
 #include <windows.h>
@@ -10,60 +12,75 @@
 #define BOARD_SIZE_Y 50
 
 void setWindowParams();
-void init(bool* cells, size_t xSize, size_t ySize);
-void generateToadOscillators(bool cells[][BOARD_SIZE_Y]);
-void printGeneration(bool* cells, size_t xSize, size_t ySize);
-void printSymbol(int x, int y, char symbol);
-void calculateCellsState();
+void initDefault2DArray(bool* cells, size_t x_size, size_t y_size);
+void generateToadOscillators(bool cells[][BOARD_SIZE_X]);
+void printGeneration(bool* cells, size_t x_size, size_t y_size);
+void printSymbol(int x, int y, int symbol);
+void calculateFollowingGeneration(bool* current_generation, bool* following_generation, size_t x_size, size_t y_size);
 
 int main()
 {
     setWindowParams();
 
-    bool cells[BOARD_SIZE_X][BOARD_SIZE_Y];
+    bool current_generation[BOARD_SIZE_Y][BOARD_SIZE_X];
+    bool following_generation[BOARD_SIZE_Y][BOARD_SIZE_X];
+    bool* current_generation_ptr = &current_generation;
+    bool* following_generation_ptr = &following_generation;
+    bool* temp_storage_ptr;
 
-    init (cells, BOARD_SIZE_X, BOARD_SIZE_Y);
-    generateToadOscillators(cells, BOARD_SIZE_Y);
-    printGeneration(cells, BOARD_SIZE_X, BOARD_SIZE_Y);
+    initDefault2DArray(current_generation, BOARD_SIZE_X, BOARD_SIZE_Y);
+    initDefault2DArray(following_generation, BOARD_SIZE_X, BOARD_SIZE_Y);
+
+    generateToadOscillators(current_generation, BOARD_SIZE_Y);
+    generateToadOscillators(following_generation, BOARD_SIZE_Y);
+
+    while (true)
+    {
+        printGeneration(current_generation_ptr, BOARD_SIZE_X, BOARD_SIZE_Y);
+        calculateFollowingGeneration(current_generation_ptr, following_generation_ptr, BOARD_SIZE_X, BOARD_SIZE_Y);
+
+        temp_storage_ptr = current_generation_ptr;
+        current_generation_ptr = following_generation_ptr;
+        following_generation_ptr = temp_storage_ptr;
+    }
 }
 
 void setWindowParams()
 {
-    SMALL_RECT windowSize = { 0, 0, BOARD_SIZE_X, BOARD_SIZE_Y };
+    SMALL_RECT window_size = { 0, 0, BOARD_SIZE_X, BOARD_SIZE_Y };
     HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleWindowInfo(hOut, TRUE, &windowSize);
+    SetConsoleWindowInfo(hOut, TRUE, &window_size);
 }
-
-void init(bool* cells, size_t xSize, size_t ySize)
+    
+void initDefault2DArray(bool* cells, size_t x_size, size_t y_size)
 {
-    for (size_t x = 0; x < xSize; x++)
+    for (size_t y = 0; y < y_size; y++)
     {
-        for (size_t y = 0; y < ySize; y++)
+        for (size_t x = 0; x < x_size; x++)
         {
-            *(cells + x * ySize + y) = false;
+            *(cells + y * x_size + x) = false;
         }
     }
 }
 
-void generateToadOscillators(bool cells[][BOARD_SIZE_Y])
+void generateToadOscillators(bool current_generation[][BOARD_SIZE_X])
 {
-    cells[10][1] = true;
-
-    /*cells[11][11] = true;
-    cells[11][12] = true;
-    cells[11][13] = true;
-    cells[12][12] = true;
-    cells[12][13] = true;
-    cells[12][14] = true;*/
+    current_generation[25][51] = true;
+    current_generation[25][52] = true;
+    current_generation[25][53] = true;
+    current_generation[26][50] = true;
+    current_generation[26][51] = true;
+    current_generation[26][52] = true;
 }
 
-void printGeneration(bool* cells, size_t xSize, size_t ySize)
+void printGeneration(bool* current_generation, size_t x_size, size_t y_size)
 {
-    for (size_t x = 0; x < xSize; x++)
+    system("cls");
+    for (size_t y = 0; y < y_size; y++)
     {
-        for (size_t y = 0; y < ySize; y++)
+        for (size_t x = 0; x < x_size; x++)
         {
-            if (*(cells + x * ySize + y))
+            if (*(current_generation + y * x_size + x))
             {
                 printSymbol(x, y, 35);
             }
@@ -77,4 +94,34 @@ void printSymbol(int x, int y, int symbol)
     printf("%c", symbol);
 }
 
-void calculateCellsState(){}
+void calculateFollowingGeneration(bool* current_generation, bool* following_generation, size_t x_size, size_t y_size)
+{
+    int number_of_live_cells;
+    for (size_t y = 0; y < y_size; y++)
+    {
+        for (size_t x = 0; x < x_size; x++)
+        {
+            number_of_live_cells = 0;
+
+            for (size_t i = 0; i < 3; i++)
+            {
+                for (size_t u = 0; u < 3; u++)
+                {
+                    if (*(current_generation + y * (x_size + i) + (x + u)) == true)
+                    {
+                        number_of_live_cells++;
+                    }
+                }
+            }
+
+            if (number_of_live_cells == 3)
+            {
+                *(following_generation + y * (x_size + 1) + (x + 1)) = true;
+            }
+            else if (number_of_live_cells != 4)
+            {
+                *(following_generation + y * (x_size + 1) + (x + 1)) = false;
+            }
+        }
+    }
+}
